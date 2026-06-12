c124820ef84fade0f8b1059302587d2214eeef56
andrewoc1 <andrewoc1111@gmail.com>
2026-06-12 17:23:14 -0400

    Complete Exercise 6 Javascript

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	let newStr = "";
+	for (let i = 0; i < num; i++){
+		newStr += string;
+	}
+	return newStr;
 };
+
+console.log(repeatString("hey",3));
 
 // Do not edit below this line
 module.exports = repeatString;
