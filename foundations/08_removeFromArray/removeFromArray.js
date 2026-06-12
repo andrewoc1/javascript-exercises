e8b8e42eed21f060979fa08f9a775bcf2cd901d4
andrewoc1 <andrewoc1111@gmail.com>
2026-06-12 18:02:49 -0400

    Complete Exercise 8 Javascript

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+	let newArr = arr; 
+	for(let i = 1; i < arguments.length; i++){
+		newArr = newArr.filter(val => val != arguments[i]);
+	}
+	return newArr;
 };
+console.log(removeFromArray([1,5,3,6,7,8], 5, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
