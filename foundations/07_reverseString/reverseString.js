00b5e6ef27d804535b0ecd1d606d76075c8f6d82
andrewoc1 <andrewoc1111@gmail.com>
2026-06-12 17:29:02 -0400

    Complete Exercise 7 Javascript

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+	let newStr = "";
+	for(let i = string.length - 1; i >= 0; i--){
+		newStr += string[i];	
+	}
+	return newStr
 };
 
+console.log(reverseString("hello there"));
 // Do not edit below this line
 module.exports = reverseString;
