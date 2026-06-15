f2febba9e0f9af1f97ed4d0d7c218b739525ceb7
andrewoc1 <andrewoc1111@gmail.com>
2026-06-14 22:07:38 -0400

    Complete Exercise 9 Javascript

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+	let larger;
+	let smaller;
+	let total = 0;
+	if(num1 >= num2){
+		larger = num1;
+		smaller = num2;
+	}else{
+		larger = num2;
+		smaller = num1;
+	}
 
+	for(let i = smaller; i <= larger; i++){
+		total += i;	
+	}
+	return total;
 };
 
 // Do not edit below this line
