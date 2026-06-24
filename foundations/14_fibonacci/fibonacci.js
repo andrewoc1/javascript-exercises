cb54d7af20e2ee462ac59e7f83fcd52255072fe4
andrewoc1 <andrewoc1111@gmail.com>
2026-06-24 17:46:52 -0400

    Complete Exercise 14 Javascript

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (times) {
+  if (parseInt(times) == 0) {
+    return 0;
+  } else if (parseInt(times) == 1 || parseInt(times) == 2) {
+    return 1;
+  } else if (parseInt(times) < 0) {
+    return "OOPS";
+  }
+  let prev = 1;
+  let current = 1;
+  for (let i = 0; i < parseInt(times) - 2; i++) {
+    let mediator = current;
+    current += prev;
+    prev = mediator;
+  }
+  return current;
 };
 
 // Do not edit below this line
